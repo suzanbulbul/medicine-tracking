35ec8a85838423c49606e2b64b6205f149e5fae0
suzanbulbul <suzan.bulbul@anymile.io>
2024-02-06 11:19:16 +0300

    added scss and toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import React from "react";
+
+//Toaster
+import { Toaster } from 'react-hot-toast';
+
+//Styles
+import '../styles/style.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+      <><Component {...pageProps} /><Toaster /></>
+  );
 }
 
-export default MyApp
+export default MyApp;
